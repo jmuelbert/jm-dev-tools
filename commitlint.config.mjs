e183d611cc0e2b46937fad3c05cda147f3881df5
Jürgen Mülbert <1974963+jmuelbert@users.noreply.github.com>
2025-12-25 12:16:23 +0100

    chore: add the configs in root

---
+++ commitlint.config.mjs
@@ -0,0 +1,3 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// SPDX-FileCopyrightText: 2025-present Jürgen Mülbert <juergen.muelbert@outlook.de>
+export default { extends: ['@commitlint/config-conventional'] }
